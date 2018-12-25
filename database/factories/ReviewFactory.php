1945ceed0c0817cd9d038a31f69efb2c935fb782
Kiyarash Sanjarani Vahed <kiyarash.3tar@gmail>
2018-12-24 20:44:35 -0800

    seed product and review table

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+            return \App\Models\Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
