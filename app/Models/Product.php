e161eac5168962a9e24153e9120a85d4dd27142d
Kiyarash Sanjarani Vahed <kiyarash.3tar@gmail>
2018-12-24 21:28:40 -0800

    Relationship created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
     //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
