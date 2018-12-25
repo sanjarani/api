92801d02d4f076fb0e5e99d9dfc927116958347f
Kiyarash Sanjarani Vahed <kiyarash.3tar@gmail>
2018-12-25 13:12:57 -0800

    Make ApiResources for products

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => $this->price - round( ($this->price * $this->discount) / 100 ),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating Yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
+    }
+}
